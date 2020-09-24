f1b2f77baa32643753192f7abbfb6adb2ab2d802
Mauricio Gouveia <mauricioargj@gmail.com>
2020-09-24 01:01:28 -0300

    Primeiro Commit

---
+++ src/main/java/com/mauricioargj/alunoscurso/AlunosCursoApplication.java
@@ -0,0 +1,13 @@
+package com.mauricioargj.alunoscurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunosCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunosCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mauricioargj/alunoscurso/AlunosCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricioargj.alunoscurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunosCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
